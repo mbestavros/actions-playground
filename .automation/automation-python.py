50c2f337a4f1050aadff1e1ccfaa54a51341c4ad
Mark Bestavros <mbestavr@redhat.com>
2020-03-20 12:20:45 -0400

    Final fixups

--- .automation/automation-python.py
+++ .automation/automation-python.py
@@ -14,35 +14,30 @@ g = Github(token)
 # Initialize repo
 repo = g.get_repo(repository)
 
-# Extract PR number from event JSON
-# Technique shown here: https://github.com/actions/checkout/issues/58
+# --- Extract PR from event JSON ---
+# `check_run`/`check_suite` does not include any direct reference to the PR
+# that triggered it in its event JSON.
+# We have to extrapolate it using the head SHA that *is* there.
 with open(event_path) as f:
     event = json.load(f)
 
-# However, since we're using `check_run`/`check_suite`, the PR number isn't there.
-# We need to look up which PR in the repo matches the head SHA included there.
-
 # Get head SHA from event JSON
 pr_head_sha = event["check_suite"]["head_sha"]
-print("PR HEAD SHA EXTRACTED FROM JSON:")
-print(pr_head_sha)
 
 # Get all repo pull requests
 repo_pull_requests = repo.get_pulls()
 
 # Find the repo PR that matches the head SHA we found
-print("POTENTIAL PRS")
 for potential_pr in repo_pull_requests:
-    print(potential_pr.head.sha)
     if potential_pr.head.sha == pr_head_sha:
         pr = potential_pr
 
 # Extract associated issue from closing keyword in PR
 closing_keyword_regex = re.compile("Resolves #\d+")
 closing_keyword = closing_keyword_regex.search(pr.body).group()
-issue_number_regex = re.compile("\d+")
-issue_number = int(issue_number_regex.search(closing_keyword).group())
-associated_issue = repo.get_issue(issue_number)
+associated_issue_number_regex = re.compile("\d+")
+associated_issue_number = int(associated_issue_number_regex.search(closing_keyword).group())
+associated_issue = repo.get_issue(associated_issue_number)
 
 # Get labels for both PR and associated issue
 issue_labels = associated_issue.labels
@@ -52,5 +47,7 @@ unset_issue_labels = []
 for label in issue_labels:
     if label not in pr_labels:
         unset_issue_labels += [label]
-    
-pr.set_labels(*unset_issue_labels)
+
+if len(unset_issue_labels) > 0:
+    labels_to_set = pr_labels + unset_issue_labels
+    pr.set_labels(*labels_to_set)
