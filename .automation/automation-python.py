ce95378b94f0015fa8bb5dfe876a01ade211b7d6
Mark Bestavros <mbestavr@redhat.com>
2020-03-20 11:54:30 -0400

    Amend rEADME

--- .automation/automation-python.py
+++ .automation/automation-python.py
@@ -8,36 +8,34 @@ token = sys.argv[1]
 repository = sys.argv[2]
 event_path = sys.argv[3]
 
+# Authenticate with Github using our token
+g = Github(token)
+
+# Initialize repo
+repo = g.get_repo(repository)
+
 # Extract PR number from event JSON
 # Technique shown here: https://github.com/actions/checkout/issues/58
 with open(event_path) as f:
     event = json.load(f)
 
+# However, since we're using `check_run`/`check_suite`, the PR number isn't there.
+# We need to look up which PR in the repo matches the head SHA included there.
 
+# Get head SHA from event JSON
+pr_head_sha = event["check_suite"]["head_sha"]
+print("PR HEAD SHA EXTRACTED FROM JSON:")
+print(pr_head_sha)
 
+# Get all repo pull requests
+repo_pull_requests = repo.get_pulls()
 
-pr_number = 19#event["number"]
-
-# Authenticate with Github using our token
-g = Github(token)
-
-# Initialize repo and grab the pull request we're working on
-repo = g.get_repo(repository)
-
-pull_requests = repo.get_pulls()
-
-for potential_pr in pull_requests:
-    print("potential_pr number:")
-    print(potential_pr.number)
-    print("head sha:")
-    print(potential_pr.head.sha)
-    print("head ref:")
-    print(potential_pr.head.sha)
-
-
-
-
-pr = repo.get_pull(pr_number)
+# Find the repo PR that matches the head SHA we found
+print("POTENTIAL PRS")
+for potential_pr in repo_pull_requests:
+    print(potential_pr.head_sha)
+    if potential_pr.head.sha == pr_head_sha:
+        pr = potential_pr
 
 # Extract associated issue from closing keyword in PR
 closing_keyword_regex = re.compile("Resolves #\d+")
