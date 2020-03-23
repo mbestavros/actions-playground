f145d532dbbde0d62b267d5237bdb765cdaf58ec
Mark Bestavros <mbestavr@redhat.com>
2020-03-23 15:12:14 -0400

    fix

--- .automation/automation-python.py
+++ .automation/automation-python.py
@@ -41,7 +41,8 @@ closing_numbers_pr_body = {number for keyword, number in regex_pr_body.findall(p
 
 # Extract all associated issues from PR commit messages
 regex_commit_messages = re.compile("(close[sd]?|fix|fixe[sd]?|resolve[sd]?|related)\s*:?\s+#(\d+)", re.I)
-closing_numbers_commit_messages = {(regex_commit_messages.findall(commit.commit.message))[1] for commit in pr.get_commits()}
+closing_numbers_commit_messages_results = {regex_commit_messages.findall(commit.commit.message) for commit in pr.get_commits()}
+closing_numbers_commit_messages = {number for keyword, number in closing_numbers_commit_messages_results}
 
 # Get the union of both sets of associated issue numbers
 closing_numbers = closing_numbers_pr_body.union(closing_numbers_commit_messages)
