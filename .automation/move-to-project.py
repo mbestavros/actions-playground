4e41934e64a540c7414bf2a88483340ef018e082
Mark Bestavros <mbestavr@redhat.com>
2020-03-24 15:19:01 -0400

    implement labels to projects

--- .automation/move-to-project.py
+++ .automation/move-to-project.py
@@ -2,14 +2,13 @@
 
 import itertools
 import json
-import re
 import sys
 from github import Github
 
 # We don't want to copy all labels on linked issues; only those in this subset.
-COPYABLE_LABELS = {
-    "enhancement",
-    "good first issue",
+LABEL_PROJECTS = {
+    "good first issue": "Example project",
+    "question": "Another example project"
 }
 
 # Get inputs from shell
@@ -28,4 +27,27 @@ with open(path) as f:
 # Determine whether the labels were added or removed
 action = event["action"]
 
-print(action)
+# Get the name of the label
+label_name = event["label"]["name"]
+
+# Determine content type: issue or PR, as well as content ID
+if "issue" in event:
+    content_type = "Issue"
+    content_id = event["issue"]["id"]
+else:
+    content_type = "PullRequest"
+    content_id = event["pull_request"]["id"]
+
+# Fetch the project we want to add to or remove from
+project = [p for p in repo.get_projects() if p.name == LABEL_PROJECTS[label_name]][0]
+
+# If the label was added, we want to add to the project. If removed, we want to
+# remove from the project.
+if(action == "labeled"):
+    project_column = project.get_columns()[0]
+    project_column.create_card(content_id=content_id, content_type=content_type)
+elif(action == "unlabeled"):
+    for column in project.get_columns():
+        for card in column.get_cards():
+            if card.id == content_id:
+                card.delete()
