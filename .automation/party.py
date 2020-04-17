061365379946b9664d71eca2000f1155bef3438e
Mark Bestavros <mbestavr@redhat.com>
2020-04-17 16:57:29 -0400

    random selection

--- .automation/party.py
+++ .automation/party.py
@@ -3,9 +3,21 @@
 
 import itertools
 import json
+import random
 import re
 import sys
 from github import Github
+
+# We don't want to copy all labels on linked issues; only those in this subset.
+ONESHOT_LINKS = {
+    "https://giant.gfycat.com/SpeedyAmazingIndianpalmsquirrel.webm",
+    "https://cdn.quotesgram.com/img/3/48/841275503-tumblr_mfx1r9sZNM1qa601io6_250.gif",
+    "https://i.imgur.com/Cn9zAnI.gif",
+    "https://66.media.tumblr.com/d9a3ce2eb39c98d1f81dfc8b5ad266f8/tumblr_mv4uuywedA1rw4rkuo4_r2_400.gif",
+    "https://www.picgifs.com/reaction-gifs/reaction-gifs/it-crowd/picgifs-it-crowd-82110.gif",
+}
+
+LATE_NIGHT = "https://thumbs.gfycat.com/FlawlessSimplisticGoldenmantledgroundsquirrel-size_restricted.gif"
 
 # Returns a pull request extracted from Github's event JSON.
 def get_pr(event):
@@ -36,5 +48,7 @@ with open(path) as f:
 # Get the PR we're working on.
 pr = get_pr(event)
 
+# Oneshot
 if(pr.review_comments == 0):
-    pr.create_issue_comment("![Well done!](https://www.picgifs.com/reaction-gifs/reaction-gifs/it-crowd/picgifs-it-crowd-82110.gif)")
+    gif = random.sample(ONESHOT_LINKS, 1)
+    pr.create_issue_comment("Congratulations on a lightning-fast merge!<br/>![Well done!](" + gif + ")")
