04d4074dc895f942daa2f1d361bca7690b11416c
Mark Bestavros <mbestavr@redhat.com>
2020-04-22 14:48:44 -0400

    fix

--- .tests/linked-issues.py
+++ .tests/linked-issues.py
@@ -14,8 +14,6 @@ def get_pr(event):
     # --- Extract issue from event JSON ---
     # `pull_request` directly refers the the PR number in the event JSON.
     # Extract that number and use it to grab the PR.
-
-    # Get PR number
     pr_number = event["number"]
 
     # Grab the PR from the number and return
@@ -25,13 +23,13 @@ def get_pr(event):
 def get_linked_issues_commits(pr):
     for c in pr.get_commits():
         for verb, num in LINKED_ISSUES.findall(c.commit.message):
-            yield int(num)
+            yield num
 
 # Extract all associated issues linked in PR description
 def get_linked_issues_body(pr):
     # Extract all associated issues from closing keyword in PR
     for verb, num in LINKED_ISSUES.findall(pr.body):
-        yield int(num)
+        yield num
 
 # Get inputs from shell
 (token, repository, path) = sys.argv[1:4]
@@ -48,17 +46,13 @@ pr = get_pr(event)
 
 commits_issues = set(get_linked_issues_commits(pr))
 description_issues = set(get_linked_issues_body(pr))
-print(commits_issues)
-print(description_issues)
-
 unlinked_issues = commits_issues - description_issues
-print(unlinked_issues)
 
 if len(unlinked_issues) > 0:
     print("Your PR contains one or more commit messages that refer to closing issues, but those issues are not linked in your pull request description.")
     print("Please include the following in your PR description:\n")
     for n in unlinked_issues:
-        print("Resolves #" + str(n))
+        print("Resolves #" + n)
     print("\nYou may use any of Github's supported closing keywords in place of 'Resolves':")
-    print("\nhttps://help.github.com/en/github/managing-your-work-on-github/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword")
+    print("https://help.github.com/en/github/managing-your-work-on-github/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword")
     exit(1)
